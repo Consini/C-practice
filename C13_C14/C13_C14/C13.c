8f0940467bd105ec9fdc9fbf412e210933563b0c
Consini <2459971164@qq.com>
2019-04-24 22:39:43 +0800

    C

--- C13_C14/C13_C14/C13.c
+++ C13_C14/C13_C14/C13.c
@@ -1,88 +1,141 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
-//#include<stdio.h>
-//#include<stdlib.h>
-//int Find(const int a[][3], int k)
-//{
-//	int i = 0;
-//	int j = 2;
-//	if (k < a[0][0] || k > a[3][3]){
-//		return 0;
-//	}
-//	while (i<3 && j>=0){
-//		if (k > a[i][j]){
-//			i++;
-//		}
-//		else if (k < a[i][j]){
-//			j--;
-//		}
-//		else{
-//			return k;
-//		}
-//	}
-//	return 0;
-//}
-//int main()
-//{
-//	int a[3][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-//	int k = 0;
-//	int ret = 0;
-//	scanf("%d", &k);
-//	ret = Find(a, k);
-//	if (ret){
-//		printf("Find the number!\n");
-//	}
-//	else
-//	{
-//		printf("Not find the number!\n");
-//	}
-//	system("pause");
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS 1
+#if 0
+//剑指offer
+//杨氏矩阵
+#include<stdio.h>
+#include<stdlib.h>
+int Find(const int a[][3], int k)
+{
+	int i = 0;
+	int j = 2;
+	if (k < a[0][0] || k > a[3][3]){
+		return 0;
+	}
+	while (i<3 && j>=0){
+		if (k > a[i][j]){
+			i++;
+		}
+		else if (k < a[i][j]){
+			j--;
+		}
+		else{
+			return k;
+		}
+	}
+	return 0;
+}
+void Print(int *arr, int row, int col)
+{
+	for (int i = 0; i < row; i++){
+		for (int j = 0; j < col; j++){
+			printf("%d ", arr[i*col + j]);
+		}
+	}
+}
+int main()
+{
+	int a[3][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+	int k = 0;
+	int ret = 0;
+	Print((int *)a, 3, 3);
+	scanf("%d", &k);
+	ret = Find(a, k);
+	if (ret){
+		printf("Find the number!\n");
+	}
+	else
+	{
+		printf("Not find the number!\n");
+	}
+	system("pause");
+	return 0;
+}
+
+
+
+
 
 
 //调整数组使奇数全部都位于偶数前面。 
-//#include<stdio.h>
-//#include<stdlib.h>
-//void Adjust(int *arr, int len)
-//{
-//	int *left = arr;
-//	int *right = arr+len-1;
-//	while (left < right)
-//	{
-//		while (*left % 2 == 1){
-//			left++;
-//		}
-//		while (*right % 2 == 0){
-//			right--;
-//		}
-//		if (left < right){
-//			int tmp = 0;
-//			tmp = *right;
-//			*right = *left;
-//			*left = tmp;
-//			left++;
-//			right--;
-//		}
-//	}
-//}
-//int main()
-//{
-//	int arr[10] = { 0 };
-//	printf("请输入数组元素:");
-//	int len = sizeof(arr) / sizeof(arr[0]);
-//	for (int i = 0; i < len; i++)
-//	{
-//		scanf("%d", &arr[i]);
-//	}
-//	Adjust(arr, len);
-//	for (int i = 0; i < len; i++)
-//	{
-//		printf("%d ", arr[i]);
-//	}
-//	system("pause");
-//	return 0;
-//}
+#include<stdio.h>
+#include<stdlib.h>
+void Adjust(int *arr, int len)
+{
+	int *left = arr;
+	int *right = arr+len-1;
+	while (left < right)
+	{
+		while ((*left % 2 == 1) && (left < right)){
+			left++;
+		}
+		while ((*right % 2 == 0) &&(left < right)){
+			right--;
+		}
+		if (left < right){
+			int tmp = 0;
+			tmp = *right;
+			*right = *left;
+			*left = tmp;
+		}
+	}
+}
+int main()
+{
+	int arr[10] = { 0 };
+	printf("请输入数组元素:");
+	int len = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < len; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	Adjust(arr, len);
+	for (int i = 0; i < len; i++)
+	{
+		printf("%d ", arr[i]);//*(arr+i)   []自带解引用
+	}
+	system("pause");
+	return 0;
+}
+#endif
 
+//调整数组使奇数全部都位于偶数前面。 
+#include<stdio.h>
+#include<stdlib.h>
+void Tiaozheng(int* arr, int len) 
+{
+	int* left = arr;
+	int* right = arr+len - 1;
+	while (left < right){
+		while (*left % 2 == 1 && left < right){
+			left++;
+		}
+		while (left < right && *right % 2 == 0){
+			right--;
+		}
+		if (left < right){
+			int tmp = *right;
+			*right = *left;
+			*left = tmp;
+		}
+	}
+	return;
+}
+void Show(int * arr, int len)
+{
+	int i = 0;
+	for (i = 0; i < len; i++){
+		printf("%d ", *(arr + i));
+	}
+}
+int main()
+{
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	Tiaozheng(arr, len);
+	Show(arr, len);
+	system("pause");
+	return 0;
+}
 
 
 
--- C13_C14/C13_C14/C14.c
+++ C13_C14/C13_C14/C14.c
@@ -1,63 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-//判断一个字符串是否为另外一个字符串旋转之后的字符串。 
-//给定s1 =AABCD和s2 = BCDAA，返回1 
-//给定s1 = abcd和s2 = ACBD，返回0
-//#include<stdio.h>
-//#include<stdlib.h>
-//#include<string.h>
-//void Fun(char *s1,int k,int len)
-//{
-//		char *start = s1;
-//		char *end = s1 + len - 1;
-//		char tmp = 0;
-//		while (k){
-//			tmp = *start;
-//			for (int i = 0; i < len - 1; i++){
-//				*(start + i) = *(start + i + 1);
-//			}
-//			*end = tmp;
-//			Fun(s1,k - 1,len);
-//			return s1;
-//		}
-//}
-//int Judge(char s1[], char s2[],int len)
-//{
-//	for (int i = 1; i < len; i++){
-//		Fun(s1,i,len);
-//		if (strcmp(s1, s2) == 1){
-//			return 1;
-//		}
-//	}
-//	return 0;
-//}
-//int main()
-//{
-//	char s1[20];
-//	char s2[20];
-//	scanf("%s", &s1);
-//	scanf("%s", &s2);
-//	int len1 = strlen(s1);
-//	int len2 = strlen(s2);
-//	int ret = 0;
-//	if (len1 == len2)
-//	{
-//		ret = Judge(s1, s2,len1);
-//		if (1 == ret){
-//			printf("s1是s2旋转之后的字符串。\n");
-//		}
-//		else{
-//			printf("s1不是s2旋转之后的字符串。\n");
-//		}
-//	}
-//	else{
-//		printf("s1不是s2旋转之后的字符串。\n");
-//	}
-//	system("pause");
-//	return 0;
-//}
 
 
+#if 0
 //ABCD\0     BCDA   CDAB
 //实现一个函数，可以左旋字符串中的k个字符。 
 #include<stdio.h>
@@ -84,14 +29,170 @@ char Fun(char *str, int k)
 	}
 	return 0;
 }
+void Left_move(char *str, int k)
+{
+	if (k < 0 || k >strlen(str)){
+		printf("k值不正确\n");
+		return;
+	}
+	while (k != 0){
+		char *cur = str;
+		char tmp = 0;
+		tmp = *cur;
+		while (*(cur + 1) != '\0'){
+			*cur = *(cur + 1);
+			cur++;
+		}
+		*cur= tmp;
+		k--;
+	}
+	return;
+}
+//ABCDEF   BACDEF  BAFEDC   CDEFAB
+void Reverse(char* left, char* right)
+{
+	assert(left != NULL && right != NULL);
+	while (left < right){
+		char tmp = *right;
+		*right = *left;
+		*left = tmp;
+		left++;
+		right--;
+	}
+}
+void leftmove(char* str, int k)
+{
+	int len = strlen(str);
+	Reverse(str, str + k - 1);
+	Reverse(str + k, str + len - 1);
+	Reverse(str, str+len-1);
+	return;
+}
 int main()
 {
-	char str[20];
-	int k = 0;
-	scanf("%s", &str);
-	scanf("%d", &k);
-	Fun(str, k);
+	char str[]="ABCDEF";
+	int k = 2;
+	char* left = str;
+	char* right =str+strlen(str)-1 ;
+	/*Fun(str, k);
+	printf("%s\n", str);*/
+	/*Left_move(str, k);
+	printf("%s\n", str);*/
+	leftmove(str, k);
 	printf("%s\n", str);
 	system("pause");
 	return 0;
 }
+
+
+
+
+
+//判断一个字符串是否为另外一个字符串旋转之后的字符串。 
+//给定s1 =AABCD和s2 = BCDAA，返回1 
+//给定s1 = abcd和s2 = ACBD，返回0
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void Fun(char *s1, int k, int len)
+{
+	char *start = s1;
+	char *end = s1 + len - 1;
+	char tmp = 0;
+	while (k){
+		tmp = *start;
+		for (int i = 0; i < len - 1; i++){
+			*(start + i) = *(start + i + 1);
+		}
+		*end = tmp;
+		Fun(s1, k - 1, len);
+		return s1;
+	}
+}
+int Judge(char s1[], char s2[], int len)
+{
+	for (int i = 1; i < len; i++){
+		Fun(s1, i, len);
+		if (strcmp(s1, s2) == 1){
+			return 1;
+		}
+	}
+	return 0;
+}
+int main()
+{
+	char s1[20];
+	char s2[20];
+	scanf("%s", &s1);
+	scanf("%s", &s2);
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+	int ret = 0;
+	if (len1 == len2)
+	{
+		ret = Judge(s1, s2, len1);
+		if (1 == ret){
+			printf("s2是s1旋转之后的字符串。\n");
+		}
+		else{
+			printf("s2不是s1旋转之后的字符串。\n");
+		}
+	}
+	else{
+		printf("s1不是s2旋转之后的字符串。\n");
+	}
+	system("pause");
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+void Reverse(char* l, char* r)
+{
+	while (l < r){
+		char tmp = *r;
+		*r = *l;
+		*l = tmp;
+		l++;
+		r--;
+	}
+	return;
+}
+void LeftMove(char *s1,int k)
+{
+	int len = strlen(s1);
+	Reverse(s1, s1+k - 1);
+	Reverse(s1 + k, s1 + len - 1);
+	Reverse(s1, s1 + len - 1);
+	return;
+}
+int main()
+{
+	char s1[20] = "ABCDEF";
+	char s2[] = "CDJIUD";
+	if (strlen(s1) == strlen(s2))
+	{
+		strncat(s1, s1, strlen(s1));
+		if (NULL == strstr(s1, s2)){
+			printf("s2不是s1旋转之后的\n");
+		}
+		else{
+			printf("s2是s1旋转之后的\n");
+		}
+	}
+	else{
+		printf("字符串长度不同\n");
+	}
+	
+	/*int k = 3;
+	char* left = s1;
+	char* right = s1 + strlen(s1) - 1;
+	LeftMove(s1,k);
+	printf("%s\n", s1);*/
+	system("pause");
+	return 0;
+}
+#endif
