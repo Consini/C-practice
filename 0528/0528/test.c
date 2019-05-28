a5deef02c193b006e809b1f306c363f32250fdfc
Consini <2459971164@qq.com>
2019-05-28 20:49:26 +0800

    c

---
+++ 0528/0528/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//输入n个整数，找出其中最小的k个整数。
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+	//printf("%d\n", strlen("abcdef"));
+	// \32被解析成一个转义字符
+	printf("%d\n", strlen("c:\test\328\test.c"));
+
+	system("pause");
+	return 0;
+}
