7244797718252ca480860f9216858afd90018bba
Consini <2459971164@qq.com>
2019-08-16 00:13:23 +0800

    最大子序列和，唯一字符

--- 最大子序列和/最大子序列和/test.c
+++ 最大子序列和/最大子序列和/test.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 int maxSubArray(int* nums, int numsSize){
 	int sum = 0;
 	int ans = nums[0];
@@ -17,12 +18,28 @@ int maxSubArray(int* nums, int numsSize){
 	}
 	return ans;
 }
-int main(){
-	int a[10];
-	for (int i = 0; i < 10; i++){
-		scanf("%d", &a[i]);
+int FirstUniqChar(char s[],int len){
+	int a[26] = { 0 };
+	int i = 0;
+	for (i = 0; i < len; i++){
+		a[s[i] - 'a'] ++;
 	}
-	printf("最大子序列和为 %d\n",maxSubArray(a, 10));
+	for (i = 0; i < len; i++){
+		if (a[s[i] - 'a'] == 1){
+			return i;
+		}
+	}
+	return -1;
+}
+int main(){
+	char str[] = "leetvodeyiqw";
+	printf("%d\n",FirstUniqChar(str,strlen(str)));
+
+	//int a[10];
+	//for (int i = 0; i < 10; i++){
+	//	scanf("%d", &a[i]);
+	//}
+	//printf("最大子序列和为 %d\n",maxSubArray(a, 10));
 	system("pause");
 	return 0;
 }
