bef2bf046f80283970699335ad89e7242b243321
Consini <2459971164@qq.com>
2019-08-25 23:11:22 +0800

    字符串压缩

---
+++ day05/day05/字符串压缩.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//char* Compress(char s[], int len){
+//	
+//}
+//void Print(char *a){
+//	while (*a != '\0'){
+//		printf("%c", *a);
+//		a++;
+//	}
+//}
+//int main(){
+//	char a[] = "xxxyyyyz";
+//	Print(a);
+//	Compress(a, strlen(a));
+//	Print(a);
+//	system("pause");
+//	return 0;
+//}
