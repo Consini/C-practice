0fb14a9c180059a7e8dff94322e6635d80700399
Consini <2459971164@qq.com>
2019-09-07 23:18:08 +0800

    词法分析

---
+++ 词法分析器.c/词法分析器.c/test.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+int i, j, k;
+char character;
+char token[6];
+
+int main(){
+	system("pause");
+	return 0;
+}
